2d6fba298a2a019ed745d5532496ebfcd9c2e7bf
dwivediaditi26-code <dwivediaditi26@gmail.com>
2026-05-06 17:23:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,24 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 5000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    target: 'esnext',
+    minify: 'esbuild',
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' },
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
 })
